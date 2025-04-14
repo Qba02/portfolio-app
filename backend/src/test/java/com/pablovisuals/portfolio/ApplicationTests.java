2d1019aa8d2250308044a9c9c54c1f0746ebcd84
Jakub Nowak <njakub002@gmail.com>
2025-04-14 10:33:49 +0200

    Backend structure names modified

--- backend/src/test/java/com/pablovisuals/portfolio/ApplicationTests.java
+++ backend/src/test/java/com/pablovisuals/portfolio/ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.portfolio.backend;
+package com.pablovisuals.portfolio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
