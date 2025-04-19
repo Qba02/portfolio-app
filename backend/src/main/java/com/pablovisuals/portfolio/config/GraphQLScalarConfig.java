b8c2fff5b4b3766d248af25d3bb6d2afc377cf44
Jakub Nowak <njakub002@gmail.com>
2025-04-19 15:17:06 +0200

    GraphQl LocalDateTime scalar added (converter and config)

---
+++ backend/src/main/java/com/pablovisuals/portfolio/config/GraphQLScalarConfig.java
@@ -0,0 +1,16 @@
+package com.pablovisuals.portfolio.config;
+
+import com.pablovisuals.portfolio.utils.conversters.LocalDateTimeScalar;
+import graphql.scalars.ExtendedScalars;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+@Configuration
+public class GraphQLScalarConfig {
+
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        return wiringBuilder -> wiringBuilder.scalar(LocalDateTimeScalar.createLocalDateTimeScalar());
+    }
+}
--- backend/src/main/java/com/pablovisuals/portfolio/model/Comment.java
+++ backend/src/main/java/com/pablovisuals/portfolio/model/Comment.java
@@ -1,19 +1,24 @@
 package com.pablovisuals.portfolio.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Document(collection = "comments")
 public class Comment {
     @Id
     private String id;
-
     private String author;
     private String message;
-    private Instant createdAt;
+    private LocalDateTime createdAt;
     private boolean approved;
 }
